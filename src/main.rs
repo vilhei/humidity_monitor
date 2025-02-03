c22f409286a9c85fe1c7aab2f96f7c77cd61a1d0
Ville Heikkinen <ville.heikkinen98@hotmail.fi>
2025-02-03 18:05:13 +0200

    Removed screen completely, now remote node and uses esp_now to communicate

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,40 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
-
-use embedded_hal_bus::i2c::CriticalSectionDevice;
+use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{
     clock::CpuClock,
     delay::Delay,
     entry,
+    gpio::Output,
     i2c::master::I2c,
-    rtc_cntl::{
-        sleep::{RtcSleepConfig, TimerWakeupSource},
-        Rtc,
-    },
-    Blocking,
+    peripherals::LPWR,
+    rng::Rng,
+    rtc_cntl::{sleep::TimerWakeupSource, Rtc},
+    timer::timg::TimerGroup,
 };
-use esp_println::println;
+use esp_wifi::esp_now::PeerInfo;
 use fugit::RateExtU32;
-use humidity_monitor::{init_display, FONT2_NORMAL, TEXT_STYLE_BIG};
-use static_cell::StaticCell;
-
-use embedded_graphics::{
-    geometry::AnchorPoint,
-    pixelcolor::BinaryColor,
-    prelude::*,
-    text::{Alignment, Text},
-};
-use u8g2_fonts::types::{FontColor::Transparent, HorizontalAlignment, VerticalPosition};
 
 use core::time::Duration;
 
-static I2C0: StaticCell<critical_section::Mutex<RefCell<I2c<Blocking>>>> = StaticCell::new();
+const CENTRAL_NODE_MAC_ADDRESS: [u8; 6] = [72, 202, 67, 207, 242, 60];
+const CENTRAL_NODE2_MAC_ADDRESS: [u8; 6] = [0x54, 0x32, 0x04, 0x47, 0xd2, 0x9c];
+
+const PERIOD_TIME: Duration = Duration::from_secs(60);
 
 #[entry]
 fn main() -> ! {
     let mut config = esp_hal::Config::default();
     config.cpu_clock = CpuClock::Clock160MHz;
     let peripherals = esp_hal::init(config);
+    measure(peripherals);
+}
 
+fn measure(peripherals: esp_hal::peripherals::Peripherals) -> ! {
+    let mut led = Output::new(peripherals.GPIO0, esp_hal::gpio::Level::Low);
+    led.set_high();
     let i2c_dev = I2c::new(
         peripherals.I2C0,
         esp_hal::i2c::master::Config {
@@ -49,59 +45,76 @@ fn main() -> ! {
     .with_sda(peripherals.GPIO3)
     .with_scl(peripherals.GPIO4);
 
-    let i2c_cs: &'static critical_section::Mutex<RefCell<I2c<Blocking>>> =
-        I2C0.init_with(|| critical_section::Mutex::new(RefCell::new(i2c_dev)));
-
-    let mut aht = aht10_embedded::AHT10::new(CriticalSectionDevice::new(i2c_cs));
+    let mut aht = aht10_embedded::AHT10::new(i2c_dev);
     aht.initialize().unwrap();
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+
+    esp_alloc::heap_allocator!(72 * 1024);
+
+    let wifi_controller = esp_wifi::init(
+        timg0.timer0,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .expect("Failed to initialize wifi");
+
+    let mut esp_now = esp_wifi::esp_now::EspNow::new(&wifi_controller, peripherals.WIFI)
+        .expect("Failed to create esp now instance");
+
+    add_esp_now_peer(&esp_now, CENTRAL_NODE_MAC_ADDRESS);
+    add_esp_now_peer(&esp_now, CENTRAL_NODE2_MAC_ADDRESS);
 
     let mut delay = Delay::new();
+
     match aht.read_data(&mut delay) {
         Ok(data) => {
             let tmp = data.temperature_celsius();
             let hum = data.humidity();
-            println!("Temp {:.3} - Humidity {:.3}", tmp, hum);
+            let msg = [&tmp.to_ne_bytes()[..], &hum.to_ne_bytes()[..]].concat();
+            send_sensor_msg(&mut esp_now, &msg, &CENTRAL_NODE_MAC_ADDRESS);
+            send_sensor_msg(&mut esp_now, &msg, &CENTRAL_NODE2_MAC_ADDRESS);
         }
-
-        Err(e) => {
-            println!("Failed to read sensor : {e:?}");
-        }
+        Err(_e) => {}
     }
-    let wakeup_source = TimerWakeupSource::new(Duration::from_secs(5));
-    let mut rtc = Rtc::new(peripherals.LPWR);
-    let mut config = RtcSleepConfig::deep();
-    // config.set_rtc_mem_inf_follow_cpu(false);
-    rtc.sleep(&config, &[&wakeup_source]);
-    unreachable!();
+
+    drop(esp_now);
+    wifi_controller
+        .deinit()
+        .expect("Failed to deinit wifi controller");
+
+    enter_deep_sleep(peripherals.LPWR, PERIOD_TIME);
 }
 
-fn render_temperature<D>(display: &mut D, temperature: f32)
-where
-    D: DrawTarget<Color = BinaryColor, Error: core::fmt::Debug>,
-{
-    FONT2_NORMAL
-        .render_aligned(
-            format_args!("{temperature:.1} °C"),
-            display.bounding_box().anchor_point(AnchorPoint::CenterLeft) + Point::new(0, -10),
-            VerticalPosition::Center,
-            HorizontalAlignment::Left,
-            Transparent(BinaryColor::On),
-            display,
-        )
-        .expect("Failed to render humidity text");
+fn send_sensor_msg(esp_now: &mut esp_wifi::esp_now::EspNow<'_>, msg: &[u8], addr: &[u8; 6]) {
+    let send_waiter = esp_now
+        .send(addr, msg)
+        .expect("failed to send esp now message")
+        .wait();
+
+    match send_waiter {
+        Ok(_) => {}
+        Err(e) => match e {
+            esp_wifi::esp_now::EspNowError::Error(_) => todo!(),
+            esp_wifi::esp_now::EspNowError::SendFailed => (),
+            esp_wifi::esp_now::EspNowError::DuplicateInstance => todo!(),
+            esp_wifi::esp_now::EspNowError::Initialization(_) => todo!(),
+        },
+    }
 }
-fn render_humidity<D>(display: &mut D, humidity: f32)
-where
-    D: DrawTarget<Color = BinaryColor, Error: core::fmt::Debug>,
-{
-    FONT2_NORMAL
-        .render_aligned(
-            format_args!("{humidity:.1} %"),
-            display.bounding_box().anchor_point(AnchorPoint::CenterLeft) + Point::new(0, 20),
-            VerticalPosition::Center,
-            HorizontalAlignment::Left,
-            Transparent(BinaryColor::On),
-            display,
-        )
-        .expect("Failed to render humidity text");
+
+fn add_esp_now_peer(esp_now: &esp_wifi::esp_now::EspNow<'_>, peer_address: [u8; 6]) {
+    esp_now
+        .add_peer(PeerInfo {
+            peer_address,
+            lmk: None,
+            channel: None,
+            encrypt: false,
+        })
+        .expect("Failed to add central node peer info");
+}
+
+fn enter_deep_sleep(lpwr: LPWR, duration: Duration) -> ! {
+    let wakeup_source = TimerWakeupSource::new(duration);
+    let mut rtc = Rtc::new(lpwr);
+    rtc.sleep_deep(&[&wakeup_source]);
 }
